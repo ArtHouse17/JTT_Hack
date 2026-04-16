f32a1c4b00efeeceb4bea2392532fb4fa43b53a5
ArtHouse17 <102299192+ArtHouse17@users.noreply.github.com>
2026-04-16 19:53:07 +0300

    Init commit

---
+++ src/main/java/com/jtt/javatachteam_hakaton/JavaTachTeamHakatonApplication.java
@@ -0,0 +1,13 @@
+package com.jtt.javatachteam_hakaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTachTeamHakatonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTachTeamHakatonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jtt/javatachteam_hakaton/JavaTachTeamHakatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtt.javatachteam_hakaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTachTeamHakatonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
